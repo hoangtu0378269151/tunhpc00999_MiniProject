beefc0656fee05ed8d0efe4675527a97946d4425
hoangtu0378269151 <90098364+hoangtu0378269151@users.noreply.github.com>
2021-09-07 14:30:43 +0700

    Delete Tunhpc00999_MiniProject directory

--- Tunhpc00999_MiniProject/src/main/java/Tunhpc00999_MiniProject/DAO/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package Tunhpc00999_MiniProject.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Tunhpc00999_MiniProject.entity.Category;
-
-public interface CategoryDAO extends JpaRepository<Category, String>{
-
-}
